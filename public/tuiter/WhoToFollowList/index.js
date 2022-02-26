9763656d4b3d44f0286da55a667c451e59f3b02c
amymin00 <amy.min7117@gmail.com>
2022-02-26 18:50:27 -0500

    fix code style

--- public/tuiter/WhoToFollowList/index.js
+++ public/tuiter/WhoToFollowList/index.js
@@ -4,7 +4,7 @@ import followUsers from "./who.js";
 const WhoToFollowList = () => {
     return (
         `<ul class="list-group d-none d-lg-block">
-        <li class="list-group-item"><strong>Who to follow</strong></li>
+            <li class="list-group-item"><strong>Who to follow</strong></li>
             ${followUsers.map(who => WhoToFollowListItem(who)).join('')}
         </ul>`
     );
