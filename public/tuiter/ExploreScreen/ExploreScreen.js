d35a81a458f607e680d6adf8ecf95b87dd96c5fa
amymin00 <amy.min7117@gmail.com>
2022-02-26 11:31:54 -0500

    fix container margin

--- public/tuiter/ExploreScreen/ExploreScreen.js
+++ public/tuiter/ExploreScreen/ExploreScreen.js
@@ -4,15 +4,14 @@ import WhoToFollowList from '../WhoToFollowList/index.js';
 
 (function ($) {
     $('#wd-explore').append(`
-        <div class="row mt-2">
-        <div class="col-2 col-md-2 col-lg-1 col-xl-2">
-            ${NavigationSidebar()}
-        </div>
-            ${ExploreComponent()}
-        <div class="d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4">
-            ${WhoToFollowList()}
-        </div>
-
+        <div class="row my-2">
+            <div class="col-2 col-md-2 col-lg-1 col-xl-2">
+                ${NavigationSidebar()}
+            </div>
+                ${ExploreComponent()}
+            <div class="d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4">
+                ${WhoToFollowList()}
+            </div>
         </div>
     `);
 })($);
