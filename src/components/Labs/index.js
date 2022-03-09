1d0da7dc35f07a9ba8d1289580a3a893ced23175
amymin00 <amy.min7117@gmail.com>
2022-03-09 10:46:04 -0500

    finished lab before ToDo list

---
+++ src/components/Labs/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Classes from "./Classes";
+import Styles from "./Styles";
+import ConditionalOutput from "./ConditionalOutput";
+
+const Labs = () => {
+    return(
+        <div>
+            <h1>Labs</h1>
+            <ConditionalOutput/>
+            <Styles/>
+            <Classes/>
+        </div>
+    )
+};
+
+export default Labs;
