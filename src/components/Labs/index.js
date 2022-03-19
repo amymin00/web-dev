6f4680e816abb23fbb29a0b571f7875b90856019
amymin00 <amy.min7117@gmail.com>
2022-03-19 16:57:43 -0400

    trying to fix tuiter styling take 1

--- src/components/Labs/index.js
+++ src/components/Labs/index.js
@@ -1,8 +1,8 @@
-import React from "react";
-import Classes from "./Classes";
-import Styles from "./Styles";
-import ConditionalOutput from "./ConditionalOutput";
-import TodoList from "./Todo/TodoList";
+import React from 'react';
+import Classes from './Classes';
+import Styles from './Styles';
+import ConditionalOutput from './ConditionalOutput';
+import TodoList from './Todo/TodoList';
 
 const Labs = () => {
     return(
@@ -14,11 +14,11 @@ const Labs = () => {
             <TodoList />
             <h1>Web Dev Assignments</h1>
             <ul>
-                <li><a href="tuiter/">Tuiter</a></li>
-                <li><a href="labs/a2/index.html">Assignment 2 Labs</a></li>
-                <li><a href="labs/a3/css/index.html">Assignment 3 Labs</a></li>
-                <li><a href="labs/a4/bootstrap/index.html">Assignment 4 Labs</a></li>
-                <li><a href="labs/a5/js/index.html">Assignment 5 Labs</a></li>
+                <li><a href='tuiter/'>Tuiter</a></li>
+                <li><a href='labs/a2/index.html'>Assignment 2 Labs</a></li>
+                <li><a href='labs/a3/css/index.html'>Assignment 3 Labs</a></li>
+                <li><a href='labs/a4/bootstrap/index.html'>Assignment 4 Labs</a></li>
+                <li><a href='labs/a5/js/index.html'>Assignment 5 Labs</a></li>
             </ul>
         </div>
     )
